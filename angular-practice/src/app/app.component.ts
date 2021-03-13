033502720b1f936e93fb8f94191399d8e0bab089
amitkumar12788 <amitkumar12788@gmail.com>
2021-03-14 01:07:47 +0530

    added

---
+++ angular-practice/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+
+  `]
+})
+export class AppComponent {
+
+}
+
+//styleUrls: ['./app.component.css']
