8c707e9d62fa11c218ee56634f183f113d0b2690
amitkumar12788 <amitkumar12788@gmail.com>
2021-03-14 13:41:44 +0530

    create new component success - warning

--- angular-practice1-basic-component/src/app/app.module.ts
+++ angular-practice1-basic-component/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
