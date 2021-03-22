0ae73372a0499461374a6d668259dee9aee292cb
amitkumar12788 <amitkumar12788@gmail.com>
2021-03-22 22:03:57 +0530

    added

--- ng-lvl4-component-databinding-deep-dive/src/app/cockpit/cockpit.component.ts
+++ ng-lvl4-component-databinding-deep-dive/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -11,7 +11,9 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
 
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+
+  @ViewChild( 'serverContentInput' ) serverContentInput: ElementRef; //'static: true'
 
   constructor() { }
 
@@ -21,14 +23,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
--- ng-lvl4-component-databinding-deep-dive/src/app/server-element/server-element.component.ts
+++ ng-lvl4-component-databinding-deep-dive/src/app/server-element/server-element.component.ts
@@ -1,18 +1,59 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component, OnInit, Input, ViewEncapsulation,
+  OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, SimpleChanges
+} from '@angular/core';
 
+// tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
 
-  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
+  @ViewChild() header: ElementRef;
 
   constructor() { }
 
+  ngOnDestroy(): void {
+    console.log('Method not implemented.');
+  }
+  ngAfterViewChecked(): void {
+    console.log('Method not implemented.');
+  }
+  ngAfterViewInit(): void {
+    console.log('Method not implemented.');
+    console.log('Text Content : ' + this.header.nativeElement.textContent);
+  }
+  ngAfterContentChecked(): void {
+    console.log('Method not implemented.');
+  }
+  ngAfterContentInit(): void {
+    console.log('Method not implemented.');
+  }
+  ngDoCheck(): void {
+    console.log('Method not implemented.');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Method not implemented.')
+    console.log(changes);
+  }
+
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+    console.log('Text Content : ' + this.header.nativeElement.textContent);
   }
 
 }
