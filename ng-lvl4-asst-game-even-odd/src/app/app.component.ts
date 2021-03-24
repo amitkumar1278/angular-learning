406b79901a5ee64c2f386bf01ac133531674626d
amitkumar12788 <amitkumar12788@gmail.com>
2021-03-24 10:16:26 +0530

    added component for odd-even game

--- ng-lvl4-asst-game-even-odd/src/app/app.component.ts
+++ ng-lvl4-asst-game-even-odd/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-lvl4-asst-game-ctrl';
+
+  onintervalFired(firedNUmber: number) {
+    console.log(firedNUmber);
+  }
 }
