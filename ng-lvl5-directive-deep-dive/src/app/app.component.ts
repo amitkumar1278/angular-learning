eaa694c265a5ca597a4e76428b151fcdaea2f7bb
amitkumar12788 <amitkumar12788@gmail.com>
2021-04-09 00:39:29 +0530

    basic directive created

--- ng-lvl5-directive-deep-dive/src/app/app.component.ts
+++ ng-lvl5-directive-deep-dive/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-lvl5-directive-deep-dive';
+	title = 'ng-lvl5-directive-deep-dive';
+	numbers = [1, 2, 3, 4, 5];
+	oddNumbers = [1, 3, 5];
+	evenNumbers = [2, 4];
+	
+	onlyOdd = false;
 }
--- ng-lvl5-directive-deep-dive/src/app/app.module.ts
+++ ng-lvl5-directive-deep-dive/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		BasicHighlightDirective
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpClientModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
