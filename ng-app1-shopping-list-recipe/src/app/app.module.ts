82933f04b91a916403587da5cf260d78c18e9e2f
amitkumar12788 <amitkumar12788@gmail.com>
2021-04-11 13:05:19 +0530

    dropdown directive

--- ng-app1-shopping-list-recipe/src/app/app.module.ts
+++ ng-app1-shopping-list-recipe/src/app/app.module.ts
@@ -10,23 +10,25 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/dropdown-directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		RecipesComponent,
+		RecipeListComponent,
+		RecipeDetailComponent,
+		RecipeItemComponent,
+		ShoppingListComponent,
+		ShoppingEditComponent,
+		DropdownDirective
+	],
+	imports: [
+		BrowserModule,
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ng-app1-shopping-list-recipe/src/app/shared/dropdown-directive.ts
@@ -0,0 +1,37 @@
+/*import { Directive, HostBinding, HostListener } from "@angular/core";
+
+
+@Directive({
+	selector: '[appDropdown]'
+})
+export class DropdownDirective {
+	
+	@HostBinding('class.open') isOpen = false;
+
+	@HostListener('click') toggleOpen() {
+		this.isOpen = !this.isOpen;
+	}
+}*/
+
+
+
+import {Directive, HostListener, HostBinding, ElementRef, Renderer2} from '@angular/core';
+ 
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+ 
+  constructor (private el: ElementRef, private renderer: Renderer2) {}
+ 
+  @HostBinding('class.show') isOpen = false;
+ 
+  @HostListener('click') toggleOpen() {
+      this.isOpen = !this.isOpen;
+      let part = this.el.nativeElement.querySelector('.dropdown-menu');
+      if(this.isOpen)
+        this.renderer.addClass(part, 'show');
+      else
+        this.renderer.removeClass(part, 'show');
+  }
+}
