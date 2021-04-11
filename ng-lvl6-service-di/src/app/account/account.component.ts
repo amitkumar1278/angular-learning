4a80161b513a6ee642c2d56489db9e01b3cd0dcd
amitkumar12788 <amitkumar12788@gmail.com>
2021-04-12 00:46:01 +0530

    creating account

---
+++ ng-lvl6-service-di/src/app/account/account.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+	selector: 'app-account',
+	templateUrl: './account.component.html',
+	styleUrls: ['./account.component.css']
+})
+export class AccountComponent {
+	@Input() account: { name: string, status: string };
+	@Input() id: number;
+	@Output() statusChanged = new EventEmitter<{ id: number, newStatus: string }>();
+
+
+	onSetTo(status: string) {
+		this.statusChanged.emit({ id: this.id, newStatus: status });
+		console.log('A server status changed, new status: ' + status);
+		console.log('Account : ' + this.account + ' , id of new server status: ' + this.id);
+	}
+}
