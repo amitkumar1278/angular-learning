a3816f412a817c1112322d5b5d02d3044ff3921f
amitkumar12788 <amitkumar12788@gmail.com>
2021-04-13 14:36:54 +0530

    create service for recipe

--- ng-app1-shopping-list-recipe/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ ng-app1-shopping-list-recipe/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+	selector: 'app-recipe-item',
+	templateUrl: './recipe-item.component.html',
+	styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
 
-  @Input() recipe: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
-  constructor() { }
+	@Input() recipe: Recipe;
 
-  ngOnInit(): void {
-  }
+	constructor(private recipeService: RecipeService) { }
 
-  // tslint:disable-next-line: typedef
-  onSelected() {
-    this.recipeSelected.emit();
-  }
+	ngOnInit(): void {
+	}
+
+	onSelected() {
+		this.recipeService.recipeSelected.emit(this.recipe);
+	}
 
 }
--- ng-app1-shopping-list-recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng-app1-shopping-list-recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,21 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe 1', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
+	recipes: Recipe[];
 
-  constructor() { }
+	constructor(private recipeService: RecipeService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.recipes = this.recipeService.getRecipes();
+	}
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+
 }
