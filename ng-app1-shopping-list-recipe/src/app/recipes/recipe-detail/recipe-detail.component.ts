31de24abb003a14d94b7b2b0a8e5ea604d4f7e0c
amitkumar12788 <amitkumar12788@gmail.com>
2021-04-13 16:36:30 +0530

    add recipes to shopping list

--- ng-app1-shopping-list-recipe/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ng-app1-shopping-list-recipe/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+	selector: 'app-recipe-detail',
+	templateUrl: './recipe-detail.component.html',
+	styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input() recipe: Recipe;
+	@Input() recipe: Recipe;
 
-  constructor() { }
+	constructor(private recipeServices: RecipeService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	onAddToShoppingList() {
+		this.recipeServices.addIngredientsToShoppingList(this.recipe.ingredients);
+	}
 }
--- ng-app1-shopping-list-recipe/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ng-app1-shopping-list-recipe/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,9 +11,8 @@ export class ShoppingEditComponent implements OnInit {
 
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
@@ -21,7 +21,7 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-
-    this.ingredientAdded.emit(newIngredient);
+	
+	this.slService.addIngredient(newIngredient);
   }
 }
