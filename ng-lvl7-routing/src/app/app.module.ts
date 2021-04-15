a35f3a05a1309efb3b4ec4be3d13524299049a57
amitkumar12788 <amitkumar12788@gmail.com>
2021-04-16 01:22:43 +0530

    routing

--- ng-lvl7-routing/src/app/app.module.ts
+++ ng-lvl7-routing/src/app/app.module.ts
@@ -11,25 +11,8 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
-
-const appRoutes: Routes = [
-	{ path: '', component: HomeComponent },
-	{
-		path: 'users', component: UsersComponent,
-		children: [
-			{ path: ':id/:name', component: UserComponent }
-		]
-	},
-	{
-		path: 'servers', component: ServersComponent,
-		children: [
-			{ path: ':id', component: ServerComponent },
-			{ path: ':id/edit', component: EditServerComponent }
-		]
-	},
-
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
 	declarations: [
@@ -39,12 +22,13 @@ const appRoutes: Routes = [
 		ServersComponent,
 		UserComponent,
 		EditServerComponent,
-		ServerComponent
+		ServerComponent,
+		PageNotFoundComponent
 	],
 	imports: [
 		BrowserModule,
 		FormsModule,
-		RouterModule.forRoot(appRoutes)
+		AppRoutingModule
 	],
 	providers: [ServersService],
 	bootstrap: [AppComponent]
